b85e2f76fb546845d535c5a81846fbc0afbfb27e
Jeffrey Noh <jnoh12388@gmail.com>
2013-07-02 13:24:53 -0700

    Get rid of hinter

--- lib/stitcher.rb
+++ lib/stitcher.rb
@@ -2,7 +2,7 @@ class Stitcher
   def self.stitch(files=[], output)
     raise if files.empty?
 
-    command = "MP4Box -force-cat -hint"
+    command = "MP4Box -force-cat "
 
     files.each do |file|
       command << " -cat #{file}"
