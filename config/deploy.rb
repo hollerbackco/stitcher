8d4fa062151d6a474d5b51b7aff192dff52648b2
Jeffrey Noh <jnoh12388@gmail.com>
2013-10-26 17:49:53 -0700

    staging server config

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,12 +20,12 @@ set :rvm_path, '/usr/local/rvm/scripts/rvm'
 
 case ENV['to']
 when 'staging'
-  set :domain, "staging.example.com"
-  set :service_name, "worker=1"
+  set :domain, "ec2-54-242-215-39.compute-1.amazonaws.com"
+  set :service_name, "stitcher_dev=1"
   set :service_env, "development"
 else
   set :domain, "ec2-54-224-250-6.compute-1.amazonaws.com"
-  set :service_name, "stitcher=1,stitcher_dev=1"
+  set :service_name, "stitcher=1"
   set :service_env, "production"
 end
 
