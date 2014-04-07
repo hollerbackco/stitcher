3c6d7e9d8bedc007ebe729c9c508e9b2bf0b958e
stabib <sajjad@hollerback.co>
2014-04-07 16:58:11 -0400

    basic logging to file on a daily basis

--- lib/stitcher_service.rb
+++ lib/stitcher_service.rb
@@ -42,7 +42,7 @@ module StitcherService
 
   def self.create_logger
     #logger = Syslog::Logger.new("stitcher")
-    logger = Logger.new(STDOUT)
+    logger = Logger.new(ENV['LOG_FILE'], 'daily')
     logger.level = Logger::INFO
     logger.formatter = ::StitcherService::Pretty.new
     FFMPEG.logger = logger
